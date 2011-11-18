9e07afcf95178e5fa15d85146e7aa40f6c694eb1
Colm Ryan <cryan@bbn.com>
2011-11-18 11:44:46 -0500

    Just some messing with simulating Hermite polynomial pulses.

--- testScript.py
+++ testScript.py
@@ -11,36 +11,52 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 from SystemParams import SystemParams
-from Hamiltonian import Hamiltonian
+from QuantumSystems import Hamiltonian, Dissipator
 from PulseSequence import PulseSequence
-from Simulation import simulate_sequence_stack
+from Simulation import simulate_sequence_stack, simulate_sequence
+from QuantumSystems import SCQubit
 
 if __name__ == '__main__':
     
     #Setup the system
     systemParams = SystemParams()
-    systemParams.Hnat = Hamiltonian(2*np.pi*np.array([[0e9,0], [0, 5e9]], dtype = np.complex128))
+    systemParams.Hnat = Hamiltonian(2*np.pi*np.array([[0,0], [0,0]], dtype = np.complex128))
     systemParams.add_control_ham(inphase = Hamiltonian(2*np.pi*0.5*np.array([[0,1],[1,0]], dtype = np.complex128)), quadrature = Hamiltonian(2*np.pi*0.5*np.array([[0,-1j],[1j,0]], dtype = np.complex128)))
     systemParams.dim = 2
     systemParams.measurement = np.array([[1,0],[0,-1]])
     
+    #Setup the Hermite polynomial 
+    numPoints = 240
+    x = np.linspace(-2,2,numPoints)
+    #Hermite 180
+#    HermiteAmps = (1-0.956*x**2)*np.exp(-(x**2))
+    #Hermite 90
+    HermiteAmps = (1-0.677*x**2)*np.exp(-(x**2))
+    
     #Setup the pulseSequences
     pulseSeqs = []
-    for delayLength in np.linspace(0,100e-9,101):
+#    controlAmp = 1.2/0.9
+    freqs = np.linspace(-20e6,20e6,100)
+    controlAmp = 0.568/1.333
+    for offRes in freqs:
     
         tmpPulseSeq = PulseSequence()
-        tmpPulseSeq.add_control_line(freq=5.01e9, initialPhase=0)
-        tmpPulseSeq.controlAmps = 10e6*np.array([[1, 0, -1]], dtype=np.float64)
-        tmpPulseSeq.timeSteps = np.array([25e-9, delayLength, 25e-9])
+        tmpPulseSeq.add_control_line(freq=-offRes, initialPhase=0)
+        tmpPulseSeq.controlAmps = (10e6*controlAmp*HermiteAmps).reshape((1,numPoints))
+        tmpPulseSeq.timeSteps = (1/1.2e9)*np.ones(numPoints)
         tmpPulseSeq.maxTimeStep = np.Inf
-        tmpPulseSeq.H_int = Hamiltonian(2*np.pi*np.array([[0,0], [0, 5.003e9]], dtype = np.complex128))
+        tmpPulseSeq.H_int = None
         
         pulseSeqs.append(tmpPulseSeq)
         
-    results = simulate_sequence_stack(pulseSeqs, systemParams, np.array([[1,0],[0,0]]), simType='unitary')
+    systemParams.measurement = np.array([[0,1],[1,0]])
+    resultsX = simulate_sequence_stack(pulseSeqs, systemParams, np.array([[1,0],[0,0]]), simType='unitary')
+    systemParams.measurement = np.array([[0,-1j],[1j,0]])
+    resultsY = simulate_sequence_stack(pulseSeqs, systemParams, np.array([[1,0],[0,0]]), simType='unitary')
+    
     
     plt.figure()
-    plt.plot(results)
+    plt.plot(freqs/1e6,np.sqrt(resultsX**2 + resultsY**2))
     plt.show()
     
     
