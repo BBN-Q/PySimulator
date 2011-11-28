26581185f7a82cd00b6d8e34784bd171af8e4ffb
Colm Ryan <cryan@bbn.com>
2011-11-28 11:51:22 -0500

    First setup of the GRAPE code and it seems to work well. For qutrit systems it optimizes in a few seconds.

--- PulseSequence.py
+++ PulseSequence.py
@@ -3,6 +3,7 @@ Created on Nov 6, 2011
 
 @author: cryan
 '''
+import numpy as np
 
 class ControlLine(object):
     def __init__(self, freq = 0, initialPhase = 0, controlType = None):
@@ -21,11 +22,20 @@ class PulseSequence(object):
         '''
         self.controlLines = []
         self.numControls = 0
+        self.timeSteps = np.zeros(0,dtype=np.float64)
+        self.controlAmps = None
+        self.H_int = None
+        self.maxTimeStep = np.Inf
     
     def add_control_line(self, freq = 0, initialPhase = 0, controlType = None):
-        self.controlLines.append(ControlLine(freq, initialPhase))
+        self.controlLines.append(ControlLine(freq, initialPhase, controlType))
         self.numControls += 1
         
+    @property
+    def numTimeSteps(self):
+        return self.timeSteps.size
+        
+    
     
 
     
