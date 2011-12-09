64c280a4655ddbd712c3b89f4fb9fbf8b58f9866
Colm Ryan <cryan@bbn.com>
2011-12-08 20:33:46 -0500

    The usual script updates.

--- scripts/Q1Control.py
+++ scripts/Q1Control.py
@@ -27,13 +27,13 @@ from PySim.OptimalControl import optimize_pulse, PulseParams
 systemParams = SystemParams()
 
 #First the two qubits
-Q1 = SCQubit(numLevels=3, omega=4.86359e9-1e6, delta=-300e6, name='Q1', T1=5.2e-6)
+Q1 = SCQubit(numLevels=3, omega=1e6, delta=-300e6, name='Q1', T1=5.2e-6)
 systemParams.add_sub_system(Q1)
-Q2 = SCQubit(numLevels=3, omega=5.19344e9-1e6, delta=-313.656e6, name='Q2', T1=4.4e-6)
+Q2 = SCQubit(numLevels=3, omega=5.19344e9-1e6-4.8626e9, delta=-313.656e6, name='Q2', T1=4.4e-6)
 systemParams.add_sub_system(Q2)
 
 #Add a 2MHz ZZ interaction 
-systemParams.add_interaction('Q1', 'Q2', 'ZZ', -2e6)
+systemParams.add_interaction('Q1', 'Q2', 'FlipFlop', 4.3e6)
 
 #Create the full Hamiltonian   
 systemParams.create_full_Ham()
@@ -42,8 +42,8 @@ systemParams.create_full_Ham()
 X = 0.5*(Q1.loweringOp + Q1.raisingOp)
 Y = 0.5*(-1j*Q1.loweringOp + 1j*Q1.raisingOp)
 #The cross-coupling from Q1 drive to Q2
-crossCoupling12 = 0.7
-crossCoupling21 = 0.7
+crossCoupling12 = 0.66
+crossCoupling21 = 0.66
 
 #Add the Q1 drive Hamiltonians
 systemParams.add_control_ham(inphase = Hamiltonian(systemParams.expand_operator('Q1', X) + crossCoupling12*systemParams.expand_operator('Q2', X)),
@@ -73,16 +73,16 @@ systemParams.add_control_ham(inphase = Hamiltonian(systemParams.expand_operator(
 sampRate = 10*1.2e9
 timeStep = 1.0/sampRate
 
-drive1Freq = Q1.omega
-drive2Freq = Q2.omega
+drive1Freq = Q1.omega-1e6
+drive2Freq = Q2.omega-1e6
 
 pulseParams = PulseParams()
-pulseParams.timeSteps = timeStep*np.ones(1440)
+pulseParams.timeSteps = timeStep*np.ones(10*144)
 pulseParams.add_control_line(freq=-drive1Freq, initialPhase=0, bandwidth=300e6, maxAmp=100e6)
 pulseParams.add_control_line(freq=-drive1Freq, initialPhase=-pi/2, bandwidth=300e6, maxAmp=100e6)
 #pulseParams.add_control_line(freq=-drive2Freq, initialPhase=0, bandwidth=300e6, maxAmp=100e6)
 #pulseParams.add_control_line(freq=-drive2Freq, initialPhase=-pi/2, bandwidth=300e6, maxAmp=100e6)
-pulseParams.H_int = Hamiltonian(systemParams.expand_operator('Q1', np.diag(drive1Freq*np.arange(Q1.dim, dtype=np.complex128))) + systemParams.expand_operator('Q2', np.diag(drive2Freq*np.arange(Q2.dim, dtype=np.complex128))))
+pulseParams.H_int = Hamiltonian(systemParams.expand_operator('Q1', drive1Freq*Q1.numberOp) + systemParams.expand_operator('Q2', drive2Freq*Q2.numberOp))
 #pulseParams.H_int = Hamiltonian(Q1.omega*np.diag(np.arange(Q1.dim)))
 pulseParams.type = 'unitary'
 
@@ -91,7 +91,7 @@ Q2Goal[2,2] = 0
 pulseParams.Ugoal = np.kron(Q1.pauliX, Q2Goal)
 
 #Rotate Q2's desired unitary by the frame rotation
-#UFrameShift = expm(1j*72e-9*systemParams.expand_operator('Q2', np.diag((Q2.omega-Q1.omega)*np.arange(Q2.dim, dtype=np.complex128))))
+#UFrameShift = expm(-1j*np.sum(pulseParams.timeSteps)*systemParams.expand_operator('Q2', Q2.Hnat.matrix - drive2Freq*Q2.numberOp))
 #pulseParams.Ugoal = np.dot(UFrameShift, pulseParams.Ugoal)
 
 
@@ -105,8 +105,8 @@ pulseParams.rhoGoal[3,3] = 1
 optimize_pulse(pulseParams, systemParams)
 
 #Decimate the pulse down to the AWG sampling rate
-pulseParams.controlAmps = decimate(pulseParams.controlAmps, 10, n=5, axis=1)
-pulseParams.timeSteps = 1e-9*np.ones(60)
+#pulseParams.controlAmps = decimate(pulseParams.controlAmps, 10, n=5, axis=1)
+#pulseParams.timeSteps = 1e-9*np.ones(60)
 
 
 '''
--- scripts/SingleQubitControl.py
+++ scripts/SingleQubitControl.py
@@ -25,13 +25,13 @@ if __name__ == '__main__':
     systemParams = SystemParams()
     
     #First the two qubits
-    Q1 = SCQubit(numLevels=3, omega=4.863e9, delta=-300e6, name='Q1', T1=5.2e-6)
+    Q1 = SCQubit(numLevels=3, omega=4.8636e9, delta=-300e6, name='Q1', T1=5.2e-6)
     systemParams.add_sub_system(Q1)
-    Q2 = SCQubit(numLevels=3, omega=5.193e9, delta=-313.656e6, name='Q2', T1=4.4e-6)
+    Q2 = SCQubit(numLevels=3, omega=5.1934e9, delta=-313.656e6, name='Q2', T1=4.4e-6)
     systemParams.add_sub_system(Q2)
  
     #Add a 2MHz ZZ interaction 
-    systemParams.add_interaction('Q1', 'Q2', 'FlipFlop', -4.25e6)
+    systemParams.add_interaction('Q1', 'Q2', 'FlipFlop', 4.25e6)
    
     #Create the full Hamiltonian   
     systemParams.create_full_Ham()
@@ -40,8 +40,8 @@ if __name__ == '__main__':
     X = 0.5*(Q1.loweringOp + Q1.raisingOp)
     Y = 0.5*(-1j*Q1.loweringOp + 1j*Q2.raisingOp)
     #The cross-coupling from Q1 drive to Q2
-    crossCoupling12 = 0.6
-    crossCoupling21 = 0.6
+    crossCoupling12 = 0.66
+    crossCoupling21 = 0.66
     
     #Add the Q1 drive Hamiltonians
     systemParams.add_control_ham(inphase = Hamiltonian(systemParams.expand_operator('Q1', X) + crossCoupling12*systemParams.expand_operator('Q2', X)),
@@ -50,10 +50,10 @@ if __name__ == '__main__':
                                   quadrature = Hamiltonian(systemParams.expand_operator('Q1', Y) + crossCoupling12*systemParams.expand_operator('Q2', Y)))
     
     #Add the Q2 drive Hamiltonians
-    systemParams.add_control_ham(inphase = Hamiltonian(crossCoupling21*systemParams.expand_operator('Q1', X) + systemParams.expand_operator('Q2', X)),
-                                  quadrature = Hamiltonian(crossCoupling21*systemParams.expand_operator('Q1', Y) + systemParams.expand_operator('Q2', Y)))
-    systemParams.add_control_ham(inphase = Hamiltonian(crossCoupling21*systemParams.expand_operator('Q1', X) + systemParams.expand_operator('Q2', X)),
-                                  quadrature = Hamiltonian(crossCoupling21*systemParams.expand_operator('Q1', Y) + systemParams.expand_operator('Q2', Y)))
+#    systemParams.add_control_ham(inphase = Hamiltonian(crossCoupling21*systemParams.expand_operator('Q1', X) + systemParams.expand_operator('Q2', X)),
+#                                  quadrature = Hamiltonian(crossCoupling21*systemParams.expand_operator('Q1', Y) + systemParams.expand_operator('Q2', Y)))
+#    systemParams.add_control_ham(inphase = Hamiltonian(crossCoupling21*systemParams.expand_operator('Q1', X) + systemParams.expand_operator('Q2', X)),
+#                                  quadrature = Hamiltonian(crossCoupling21*systemParams.expand_operator('Q1', Y) + systemParams.expand_operator('Q2', Y)))
     
     
     #Setup the measurement operator
@@ -68,44 +68,26 @@ if __name__ == '__main__':
     #Setup the initial state as the ground state
     rhoIn = np.kron(Q1.levelProjector(0), Q2.levelProjector(1))
     
-    sampRate = 1.2e9
+    sampRate = 0.6e9
     timeStep = 1.0/sampRate
     
-    nutFreqs = 50e6*np.linspace(-1,1,81)
     drive1Freq = Q1.omega
     drive2Freq = Q2.omega
 
-
-    #Calibrate a 120ns Gaussian pulse on Q1
+    #Calibrate a 240ns Gaussian pulse on Q1
     numPoints = 144
     xPts = np.linspace(-2,2,numPoints)
     gaussPulse = np.exp(-(xPts**2))
     tmpControls = np.zeros((4,numPoints))
     tmpControls[0] = gaussPulse
-#
-#    #Setup the pulseSequences as a series of 10us low-power pulses at different frequencies
-#    pulseSeqs = []
-#    for nutFreq in nutFreqs:
-#        tmpPulseSeq = PulseSequence()
-#        tmpPulseSeq.add_control_line(freq=-drive1Freq, initialPhase=0)
-#        tmpPulseSeq.add_control_line(freq=-drive1Freq, initialPhase=-pi/2)
-#        tmpPulseSeq.add_control_line(freq=-drive2Freq, initialPhase=0)
-#        tmpPulseSeq.add_control_line(freq=-drive2Freq, initialPhase=-pi/2)
-#        tmpPulseSeq.controlAmps = nutFreq*tmpControls
-#        tmpPulseSeq.timeSteps = timeStep*np.ones(tmpControls.shape[1])
-#        tmpPulseSeq.maxTimeStep = 5e-10
-#        tmpPulseSeq.H_int = Hamiltonian(systemParams.expand_operator('Q1', np.diag(drive1Freq*np.arange(Q1.dim, dtype=np.complex128))) + systemParams.expand_operator('Q2', np.diag(drive2Freq*np.arange(Q2.dim, dtype=np.complex128))))
-#        pulseSeqs.append(tmpPulseSeq)
-#
-#    results = simulate_sequence_stack(pulseSeqs, systemParams, rhoIn, simType='unitary')[0]
-#    
+    
     #Calibrates to 9.51MHz as expected
     Q1Cal = 0.5/(np.sum(gaussPulse)*timeStep)
     bufferPts = 2
-    Q1PiPulse = np.zeros((4,numPoints))
+    Q1PiPulse = np.zeros((systemParams.numControlHams,numPoints))
     Q1PiPulse[0] = Q1Cal*gaussPulse
-    Q1PiBlock = np.hstack((np.zeros((4,bufferPts)), Q1PiPulse, np.zeros((4,bufferPts))))
-    Q1Pi2Block = np.hstack((np.zeros((4,bufferPts)), 0.5*Q1PiPulse, np.zeros((4,bufferPts))))
+    Q1PiBlock = np.hstack((np.zeros((systemParams.numControlHams,bufferPts)), Q1PiPulse, np.zeros((systemParams.numControlHams,bufferPts))))
+    Q1Pi2Block = np.hstack((np.zeros((systemParams.numControlHams,bufferPts)), 0.5*Q1PiPulse, np.zeros((systemParams.numControlHams,bufferPts))))
     Q1TimePts = timeStep*np.ones(Q1PiBlock.shape[1])
 
     
@@ -125,18 +107,43 @@ if __name__ == '__main__':
     Q2PiBlock[3,2:-2] = dragCorrection
     Q2PiBlock *= Q2Cal
     
-
+    '''
+    #Setup to look at the cross-resonance drive: i.e. whether the Q1 Rabi frequency depends on the state of Q2
+    ampSweep = np.linspace(0,3,40)
+    freqSweep = np.linspace(4.855e9,4.885e9,41)
+    freqSweep = np.array([4.862e9])
+    pulseSeqs = []
+    for freq in freqSweep:
+        for controlAmp in ampSweep:
+            tmpPulseSeq = PulseSequence()
+            tmpPulseSeq.add_control_line(freq=-freq, initialPhase=0)
+            tmpPulseSeq.add_control_line(freq=-freq, initialPhase=-pi/2)
+            tmpPulseSeq.controlAmps = controlAmp*Q1PiBlock
+            tmpPulseSeq.timeSteps = timeStep*np.ones(Q1PiBlock.shape[1])
+            tmpMat = freq*Q1.numberOp
+            tmpPulseSeq.H_int = Hamiltonian(systemParams.expand_operator('Q1', tmpMat) + systemParams.expand_operator('Q2', tmpMat))
+        
+            pulseSeqs.append(tmpPulseSeq)
+    
+    results = simulate_sequence_stack(pulseSeqs, systemParams, rhoIn, simType='lindblad')[0]
+    results.resize((freqSweep.size, ampSweep.size))
+    '''
+            
+            
+    
+    
+    
     #Run a Ramsey experiment
     #Setup the pulseSequences
     delays = np.linspace(0,8e-6,200)
     pulseSeqs = []
-    drive1Freq = 4.863e9
+    drive1Freq = 4.8626e9
     drive2Freq = 5.193e9
     
-    JStrengths = np.linspace(0.1,6e6,30)
+    JStrengths = np.linspace(4.3e6,4.3e6,1)
     RamseyFreqs = []
     for JStrength in JStrengths:
-        systemParams.interactions[0].interactionStrength = -JStrength
+        systemParams.interactions[0].interactionStrength = JStrength
         systemParams.interactions[0].createMat()
         systemParams.create_full_Ham()
         pulseSeqs = []
@@ -144,10 +151,10 @@ if __name__ == '__main__':
             tmpPulseSeq = PulseSequence()
             tmpPulseSeq.add_control_line(freq=-drive1Freq, initialPhase=0, controlType='rotating')
             tmpPulseSeq.add_control_line(freq=-drive1Freq, initialPhase=-pi/2, controlType='rotating')
-            tmpPulseSeq.add_control_line(freq=-drive2Freq, initialPhase=0, controlType='rotating')
-            tmpPulseSeq.add_control_line(freq=-drive2Freq, initialPhase=-pi/2, controlType='rotating')
+#            tmpPulseSeq.add_control_line(freq=-drive2Freq, initialPhase=0, controlType='rotating')
+#            tmpPulseSeq.add_control_line(freq=-drive2Freq, initialPhase=-pi/2, controlType='rotating')
             #Pulse sequence is X90, delay, X90
-            tmpPulseSeq.controlAmps = np.hstack((Q1Pi2Block, np.zeros((4,1)), Q1Pi2Block))
+            tmpPulseSeq.controlAmps = np.hstack((Q1Pi2Block, np.zeros((Q1PiBlock.shape[0],1)), Q1Pi2Block))
             tmpPulseSeq.timeSteps = np.hstack((Q1TimePts, np.array([delay]), Q1TimePts))
             #Interaction frame at the drive frequency
             tmpPulseSeq.H_int = Hamiltonian(systemParams.expand_operator('Q1', drive1Freq*Q1.numberOp) + systemParams.expand_operator('Q2', drive1Freq*Q2.numberOp))
@@ -173,7 +180,6 @@ if __name__ == '__main__':
         plt.plot(delays*1e6,fitfunc(p1,delays))
         plt.show()    
         RamseyFreqs.append(p1[1])
-    
     
     '''
     #Run the actual pi-pi-pi-pi experiment
